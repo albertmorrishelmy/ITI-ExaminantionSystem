cab4cba9e1c2c486f6a99ce433afdafee6b84141
Alameer Ashraf Hassan <alamiir.ashraf@gmail.com>
2017-05-24 01:02:14 +0200

    IOperations Interface
    Access Data Layer

--- API-OAuth/BusineesLayer/IOperations.cs
+++ API-OAuth/BusineesLayer/IOperations.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,21 @@ namespace BusineesLayer
 {
     interface IOperations<T> where T : class
     {
-
+        IQueryable<T> GetAll();   // 
+        T GetById(int id);
+        T FindBy(Expression<Func<bool, T>> Condition);
+        IQueryable<T> FindListBy(Expression<Func<bool, T>> Condition);  
+        T Add(T Entity);
+        bool AddRange(List<T> Entities);   
+        bool Delete(int id);
+        bool Delete(T EntityToDelete);
+        bool Update(T EntityToUpdate);
+        void Save();
+        int Count();
     }
 }
+
+
+
+// IQueryable bring only filtered data from server to the client ; 
+// IEnumerable bring all data from server to clint (High-performance); 
