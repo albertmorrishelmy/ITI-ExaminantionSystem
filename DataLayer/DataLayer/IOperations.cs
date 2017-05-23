c893f2f2d685d53559ac2805451962d32c4fef79
Alameer Ashraf Hassan <alamiir.ashraf@gmail.com>
2017-05-23 19:38:11 +0200

    Initialze DataLayer
    API Updates

---
+++ DataLayer/DataLayer/IOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    interface IOperations<T> where T : class 
+    {
+        T Get(int id);
+        List<T> GetAll();
+        List<T> FindBy(Expression<Func<T, bool>> Condition);
+        T Add(T Object);
+        List<T> AddRange(List<T> LisObj); 
+
+
+    }
+}
