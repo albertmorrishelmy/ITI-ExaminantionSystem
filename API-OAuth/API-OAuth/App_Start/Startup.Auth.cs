1fdadceb0391378d609822326a1ccaab9b13dde5
Alameer Ashraf Hassan <alamiir.ashraf@gmail.com>
2017-05-27 11:16:52 +0200

    revoke ODate Updates

--- API-OAuth/API-OAuth/App_Start/Startup.Auth.cs
+++ API-OAuth/API-OAuth/App_Start/Startup.Auth.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace API_OAuth.App_Start
+namespace API_OAuth
 {
     public partial class Startup
     {
--- API-OAuth/API-OAuth/Startup.cs
+++ API-OAuth/API-OAuth/Startup.cs
@@ -10,8 +10,8 @@ using DataAccessLayer;
 using System.Web.Http.OData.Batch;
 using Microsoft.Data.Edm;
 using System.Web.Http.OData.Builder;
-using BusineesLayer.Map;
 using DataAccessLayer.Models;
+using BusineesLayer;
 using Microsoft.Data.Edm.Library;
 using System.Web.Hosting;
 using Swashbuckle.Application;
@@ -20,14 +20,38 @@ using Swashbuckle.Application;
 
 namespace API_OAuth
 {
-    public class Startup
+    public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
+
+            // ConfigureAuth(app);
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
             HttpConfiguration config = new HttpConfiguration();
-
+            // ConfigureOAuth(app);
             ConfigureAuth(app);
-
+            WebApiConfig.Register(config);
+            
+            //app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            config.Filters.Add(new AuthorizeAttribute());
+            app.UseWebApi(config);
+            //config.AddODataQueryFilter(new InlineCountQueryableAttribute());
+            //ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            //builder.EntitySet<program>("programs");
+            //builder.EntitySet<ProgramIntake>("ProgramIntakes");
+            //builder.EntitySet<PlatfromIntake>("PlatfromIntakes");
+            ////builder.EntitySet<program>("programs");
+            //builder.EntitySet<Intake>("Intakes");
+            //builder.EntitySet<subTrack>("subTracks");
+            //builder.EntitySet<Employee>("Employees");
+            //builder.EntitySet<StudentBasicData>("StudentBasicDatas");
+            //builder.EntitySet<TrackSupervisor>("TrackSupervisors");
+            //builder.EntitySet<TrackManual>("TrackManuals");
+            //builder.EntitySet<Branch>("Branchs");
+            //config.EnableQuerySupport();
+            //config.Routes.MapODataServiceRoute("ODataRoute", "odata", builder.GetEdmModel());
+            //AutoMapperConfiguration.Configure();
         }
+      
     }
 }
