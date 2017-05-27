e0a0a2f40cc1d7f23718e29a6c800ee3587466d7
Alameer Ashraf Hassan <alamiir.ashraf@gmail.com>
2017-05-27 10:11:06 +0200

    OAuth Update Configuration

---
+++ API-OAuth/API-OAuth/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using System.Web.Http.Owin;
+using System.Web.Http.Controllers;
+using System.Web.Http.OData.Extensions;
+using DataAccessLayer;
+using System.Web.Http.OData.Batch;
+using Microsoft.Data.Edm;
+using System.Web.Http.OData.Builder;
+using BusineesLayer.Map;
+using DataAccessLayer.Models;
+using Microsoft.Data.Edm.Library;
+using System.Web.Hosting;
+using Swashbuckle.Application;
+
+[assembly: OwinStartup(typeof(API_OAuth.Startup))]
+
+namespace API_OAuth
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureAuth(app);
+
+        }
+    }
+}
